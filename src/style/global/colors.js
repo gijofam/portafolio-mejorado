e20e07dd6342fae56bae4b6b0477efe5b1d1abf0
gilmar <gipajfm@gmail.com>
2023-04-30 19:17:45 -0500

    add global style variables

---
+++ src/style/global/colors.js
@@ -0,0 +1,8 @@
+export const colors ={
+    backgroundSlideColor: '#000000',
+    ProjectAndContactColor: '#181823',
+    aboutColor:'#e40037',
+    projectColor: '#10fff1',
+    contactColor: '#0c08b1',
+    inputColor: '#494993'
+}
---
+++ src/style/global/typography.js
@@ -0,0 +1,9 @@
+export const typography = {
+    nameAndTitle: `Poppins', sans-serif;`,
+    nameSize: `40px`,
+    titleSize: `30px`,
+    text: `'Sen', sans-serif;`,
+    textSize: `22px`,
+    regular: `400;`,
+    semiBold: `600;`, 
+}
